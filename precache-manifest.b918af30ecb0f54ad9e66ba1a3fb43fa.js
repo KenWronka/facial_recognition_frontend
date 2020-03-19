d01f4ed3c53f8e878af3877b190ba50e14bc9362
KenWronka <ken.wronka@gmail.com>
2020-03-19 18:10:28 -0400

    Updates

---
+++ precache-manifest.b918af30ecb0f54ad9e66ba1a3fb43fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a8bfb05c954da95d21906de6981465",
+    "url": "/facial_recognition_frontend/index.html"
+  },
+  {
+    "revision": "67da4e038f0b1a24ed7b",
+    "url": "/facial_recognition_frontend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0f03a382c7f13fffa24c",
+    "url": "/facial_recognition_frontend/static/css/main.77f5e0c2.chunk.css"
+  },
+  {
+    "revision": "67da4e038f0b1a24ed7b",
+    "url": "/facial_recognition_frontend/static/js/2.29753be0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/facial_recognition_frontend/static/js/2.29753be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f03a382c7f13fffa24c",
+    "url": "/facial_recognition_frontend/static/js/main.9770f733.chunk.js"
+  },
+  {
+    "revision": "defac11783c85191ab9e",
+    "url": "/facial_recognition_frontend/static/js/runtime-main.762c8aeb.js"
+  }
+]);
